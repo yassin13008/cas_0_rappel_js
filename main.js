14c9ab23c626e09517d9826965eea2ec2e9bc953
Yassin Hammami <yas.hammami@gmail.com>
2022-11-23 14:41:07 +0100

    enre

--- main.js
+++ main.js
@@ -1,27 +1,40 @@
+"use strict"; // Ajout de sécurité
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    document.getElementById('xhr-button').addEventListener('click', onClickXHRButton)
+});
 
 
 
-document.addEventListener('DOMContentLoaded',()=> {
-    document.getElementById('xhr-button').addEventListener('click',onClickXHRButton)
-})
+function onClickXHRButton(){
 
+    const xhr = new XMLHttpRequest();
 
-function onClickXHRButton() {
-    const xhr = new XMLHttpRequest()
+    xhr.open('get','contenu-json.json');
+    
+    xhr.addEventListener("load", function() {
 
-    xhr.open("get","contenu-html.html");
-    xhr.addEventListener('load',()=> {
-        console.log("Statut de la reponse : " + xhr.status + " - " + xhr.statusText);
+        console.log("Statut de la réponse: "+ xhr.status + " -  " + xhr.statusText);
+
         const container = document.getElementById("xhr-response");
-        const response = xhr.responseText;
-        container.innerHTML = response;
+        
+        const jsonData = xhr.responseText;
+
+        container.innerHTML = jsonData;
+
+        const data = JSON.parse(jsonData);
+        console.log(data);
+        console.log(jsonData);
+
+    });
+    
+    xhr.addEventListener("error", function(){
+        console.log("La requte AJAX a échouée ! ☠ ")
     })
 
+    xhr.send();
+    
+}
 
 
-xhr.addEventListener("error", ()=> {
-    console.log("La requête Ajax a échoué !")
-})
-
-xhr.send();
-};
