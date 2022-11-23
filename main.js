0871f100f0e569a9678445fa4af2802cce88a4d7
Yassin Hammami <yas.hammami@gmail.com>
2022-11-23 15:43:16 +0100

    Exercice Json fait

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+// "use strict"; // Ajout de sécurité
+
+// document.addEventListener("DOMContentLoaded", function() {
+
+//     document.getElementById('xhr-button').addEventListener('click', onClickXHRButton)
+// });
+
+
+
+// function onClickXHRButton(){
+
+//     ajaxGetJson("contenu-json.json", ()=> {
+//         console.log(data)
+//     })
+// }
+
 "use strict"; // Ajout de sécurité
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -9,32 +25,6 @@ document.addEventListener("DOMContentLoaded", function() {
 
 function onClickXHRButton(){
 
-    const xhr = new XMLHttpRequest();
-
-    xhr.open('get','contenu-json.json');
-    
-    xhr.addEventListener("load", function() {
-
-        console.log("Statut de la réponse: "+ xhr.status + " -  " + xhr.statusText);
-
-        const container = document.getElementById("xhr-response");
-        
-        const jsonData = xhr.responseText;
-
-        container.innerHTML = jsonData;
-
-        const data = JSON.parse(jsonData);
-        console.log(data);
-        console.log(jsonData);
-
-    });
-    
-    xhr.addEventListener("error", function(){
-        console.log("La requte AJAX a échouée ! ☠ ")
-    })
-
-    xhr.send();
+    ajaxGetJson("contenu-json.json",(data)=>{console.log(data);});
     
 }
-
-
--- service.js
+++ service.js
@@ -1,35 +1,29 @@
-function ajaxGetJson(url, callback) {
-    
-    function onClickXHRButton(){
+function ajaxGetJson(url,callBack){
 
-        const xhr = new XMLHttpRequest();
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('get',url);
     
-        xhr.open('get','url');
+    xhr.addEventListener("load", function() {
+
+        console.log("Statut de la réponse: "+ xhr.status + " -  " + xhr.statusText);
+
+        const container = document.getElementById("xhr-response");
         
-        xhr.addEventListener("load", function() {
-    
-            console.log("Statut de la réponse: "+ xhr.status + " -  " + xhr.statusText);
-    
-            const container = document.getElementById("xhr-response");
-            
-            const jsonData = xhr.responseText;
-    
-            container.innerHTML = jsonData;
-    
-            const data = JSON.parse(jsonData);
-            
-            callback(data);
+        const jsonData = xhr.responseText;
 
+        container.innerHTML = jsonData;
 
+        const data = JSON.parse(jsonData);
 
+        callBack(data);
+
+    });
     
-        });
-        
-        xhr.addEventListener("error", function(){
-            console.log("La requte AJAX a échouée ! ☠ ")
-        })
+    xhr.addEventListener("error", function(){
+        console.log("La requte AJAX a échouée ! ☠ ")
+    })
+
+    xhr.send();
     
-        xhr.send();
-        
-    }
 }
